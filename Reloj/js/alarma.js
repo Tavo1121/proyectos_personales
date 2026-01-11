f9f64b35d5ea9380dc8c7f463dae5ed17f7550bb
Tavo1121 <fabioyaleli2005@gmail.com>
2026-01-11 23:18:34 +0100

    Actualizacion 11/01/2005
    Creación del apartado de alarma sin que las funciones se puedan ejecutar

---
+++ Reloj/js/alarma.js
@@ -0,0 +1,10 @@
+// Esta parte del js muestra por pantalla la hora actual
+const hora_actual = document.getElementById("hora_actual");
+
+function actualizarHora() {
+    const hora = new Date();
+    hora_actual.textContent= hora.toLocaleTimeString();
+}
+
+setInterval(actualizarHora, 1000);
+actualizarHora();
